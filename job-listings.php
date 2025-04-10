1ee9e7b33031adc3d30b8d25cf8ee6b8c2cf03b7
Ad4m123 <adam.jokai123@gmail.com>
2025-04-10 21:13:33 +0200

    nav

--- job-listings.php
+++ job-listings.php
@@ -1,57 +1,9 @@
 <?php include_once "parts/header.php"?>
-    
-    <body class="job-listings-page" id="top">
 
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="index.php">
-                    <img src="images/logo.png" class="img-fluid logo-image">
-
-                    <div class="d-flex flex-column">
-                        <strong class="logo-text">Gotto</strong>
-                        <small class="logo-slogan">Online Job Portal</small>
-                    </div>
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav align-items-center ms-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">Homepage</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">About Gotto</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
-
-                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item active" href="job-listings.html">Job Listings</a></li>
-
-                                <li><a class="dropdown-item" href="job-details.php">Job Details</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.php">Contact</a>
-                        </li>
-
-                        <li class="nav-item ms-lg-auto">
-                            <a class="nav-link" href="#">Register</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link custom-btn btn" href="#">Login</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<?php
+$activePage = 'jobs'; // alebo 'home', 'about', 'contact', 'job-details'
+include_once('parts/nav.php');
+?>
 
         <main>
 
