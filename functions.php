3dfd896c6473b489fd1a724bbce882d2808fe012
Ad4m123 <adam.jokai123@gmail.com>
2025-05-22 14:50:17 +0200

    Tahanie prác cez databázu

--- functions.php
+++ functions.php
@@ -11,3 +11,25 @@ function pridajPozdrav() //Pozdrav podla času
     }
 }
 $pozdrav = pridajPozdrav();
+
+function formatTimeAgo($datetime) {
+    if (!$datetime) {
+        return "Unknown time";
+    }
+
+    $createdDate = new DateTime($datetime);
+    $now = new DateTime();
+    $interval = $now->diff($createdDate);
+
+    if ($interval->y > 0) {
+        return $interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->m > 0) {
+        return $interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->d > 0) {
+        return $interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago';
+    } elseif ($interval->h > 0) {
+        return $interval->h . ' hour' . ($interval->h > 1 ? 's' : '') . ' ago';
+    } else {
+        return $interval->i . ' minute' . ($interval->i > 1 ? 's' : '') . ' ago';
+    }
+}
